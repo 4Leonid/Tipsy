1fb054b395c1e3a224ebaa3cd9946f275649825a
Leonid <leo-turko@mail.ru>
2023-04-26 21:02:22 +0300

    First view controller

--- Tipsy/MainViewController.swift
+++ Tipsy/MainViewController.swift
@@ -17,11 +17,97 @@ class MainViewController: UIViewController {
     return element
   }()
   
-  private lazy var firstStack: UIStackView = {
+  //  MARK: -  StackView
+  private lazy var fouthStack: UIStackView = {
+    let element = UIStackView()
+    element.axis = .horizontal
+    element.distribution = .fillProportionally
+    element.alignment = .fill
+    return element
+  }()
+  
+  private lazy var thirdStack: UIStackView = {
+    let element = UIStackView()
+    element.axis = .horizontal
+    element.distribution = .fillProportionally
+    element.alignment = .fill
+    return element
+  }()
+  
+  private lazy var secondStack: UIStackView = {
     let element = UIStackView()
     element.axis = .vertical
     element.spacing = 26
     element.distribution = .fill
+    element.alignment = .center
+    return element
+  }()
+  
+  private lazy var firstStack: UIStackView = {
+    let element = UIStackView()
+    element.axis = .vertical
+    element.spacing = 27
+    element.distribution = .fill
+    element.alignment = .fill
+    return element
+  }()
+  
+  //  MARK: -  UIButtons
+  private lazy var calculateButton: UIButton = {
+    let element = UIButton(type: .system)
+    element.backgroundColor = #colorLiteral(red: 0.0004900721833, green: 0.6912010908, blue: 0.4188455343, alpha: 1)
+    element.titleLabel?.tintColor = .white
+    element.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+    element.setTitle("Calculate", for: .normal)
+    return element
+  }()
+  
+  private lazy var zeroButton: UIButton = {
+    let element = UIButton(type: .system)
+    element.setTitle("0%", for: .normal)
+    element.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+    element.tintColor = #colorLiteral(red: 0.0004900721833, green: 0.6912010908, blue: 0.4188455343, alpha: 1)
+    return element
+  }()
+  
+  private lazy var tenButton: UIButton = {
+    let element = UIButton(type: .system)
+    element.setTitle("10%", for: .normal)
+    element.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+    element.tintColor = #colorLiteral(red: 0.0004900721833, green: 0.6912010908, blue: 0.4188455343, alpha: 1)
+    return element
+  }()
+  
+  private lazy var twentyButton: UIButton = {
+    let element = UIButton(type: .system)
+    element.setTitle("20%", for: .normal)
+    element.titleLabel?.font = UIFont.systemFont(ofSize: 35)
+    element.tintColor = #colorLiteral(red: 0.0004900721833, green: 0.6912010908, blue: 0.4188455343, alpha: 1)
+    return element
+  }()
+  
+  //  MARK: -  UILabel
+  private lazy var splitNumberLabel: UILabel = {
+    let element = UILabel()
+    element.text = "2"
+    element.textColor = #colorLiteral(red: 0.0004900721833, green: 0.6912010908, blue: 0.4188455343, alpha: 1)
+    element.font = UIFont.systemFont(ofSize: 35)
+    return element
+  }()
+  
+  private lazy var tipLabel: UILabel = {
+    let element = UILabel()
+    element.text = "Select tip"
+    element.textColor = .lightGray
+    element.font = UIFont.systemFont(ofSize: 25)
+    return element
+  }()
+  
+  private lazy var splitLabel: UILabel = {
+    let element = UILabel()
+    element.text = "Choose split"
+    element.textColor = .lightGray
+    element.font = UIFont.systemFont(ofSize: 25)
     return element
   }()
   
@@ -30,6 +116,12 @@ class MainViewController: UIViewController {
     element.text = "Enter bill total"
     element.textColor = .lightGray
     element.font = UIFont.systemFont(ofSize: 25)
+    return element
+  }()
+  
+  private lazy var stepper: UIStepper = {
+    let element = UIStepper()
+    
     return element
   }()
   
@@ -53,19 +145,56 @@ extension MainViewController {
   private func setViews() {
     view.addSubview(mainView)
     view.addSubview(firstStack)
+    view.addSubview(calculateButton)
+    
+    mainView.addSubview(secondStack)
+    mainView.addSubview(thirdStack)
+    mainView.addSubview(fouthStack)
     
     firstStack.addArrangedSubview(billLabel)
     firstStack.addArrangedSubview(moneyTextField)
+    
+    secondStack.addArrangedSubview(tipLabel)
+    secondStack.addArrangedSubview(thirdStack)
+    secondStack.addArrangedSubview(splitLabel)
+    secondStack.addArrangedSubview(fouthStack)
+    
+    thirdStack.addArrangedSubview(zeroButton)
+    thirdStack.addArrangedSubview(tenButton)
+    thirdStack.addArrangedSubview(twentyButton)
+    
+    fouthStack.addArrangedSubview(splitNumberLabel)
+    fouthStack.addArrangedSubview(stepper)
   }
   
   private func setConstraints() {
+    calculateButton.snp.makeConstraints { make in
+      make.bottom.equalTo(view.safeAreaLayoutGuide)
+      make.width.greaterThanOrEqualTo(200)
+      make.centerX.equalTo(view.snp.centerX)
+      make.height.equalTo(54)
+    }
+    thirdStack.snp.makeConstraints { make in
+      make.leading.trailing.equalToSuperview()
+    }
+    splitLabel.snp.makeConstraints { make in
+      make.leading.height.equalTo(30)
+    }
+    tipLabel.snp.makeConstraints { make in
+      make.leading.height.equalTo(30)
+    }
+    
+    secondStack.snp.makeConstraints { make in
+      make.leading.trailing.top.equalToSuperview().inset(20)
+    }
+    
     moneyTextField.snp.makeConstraints { make in
       make.height.equalTo(48)
       make.leading.trailing.equalToSuperview()
     }
     
     billLabel.snp.makeConstraints { make in
-      make.leading.trailing.equalToSuperview().inset(50)
+      make.leading.equalToSuperview().inset(50)
       make.height.equalTo(30)
     }
     
